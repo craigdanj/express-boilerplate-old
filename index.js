e295be44986dc0a9ac4d3c332bdc437736dd9e48
Craig <craigdanj@gmail.com>
2020-03-24 01:10:34 +0530

    util for rootDir added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const rootDir = require('./utils/path');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
@@ -19,7 +20,7 @@ app.use(shopRoutes);
 
 app.use((req, res ,next) => {
     //The 404 route
-    res.status(404).sendFile(path.join(__dirname,'views', '404.html'));
+    res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
 });
 
 app.listen(3000);
