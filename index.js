af6e1c31a0e3953adba9fa02de488a7049d44c4b
Craig <craigdanj@gmail.com>
2020-03-24 00:34:35 +0530

    a 404 route added

--- index.js
+++ index.js
@@ -16,4 +16,9 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(adminRoutes);
 app.use(shopRoutes);
 
+app.use((req, res ,next) => {
+    //The 404 route
+    res.status(404).send('<h1>404: Page not found!</1>');
+});
+
 app.listen(3000);
