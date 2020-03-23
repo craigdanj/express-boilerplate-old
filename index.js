d560358f55061a348c45f21e6eb17c1ed18f8707
Craig <craigdanj@gmail.com>
2020-03-23 23:26:22 +0530

    basic routes added

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('boom bam');
+const express = require('express');
+
+const app = express();
+
+app.use('/', (req, res, next) => {
+    console.log('root');
+    next();
+});
+
+app.use('/test', (req, res, next) => {
+    console.log('test');
+    next();
+});
+
+app.listen(3000);
