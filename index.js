b8bb91fe6c3b8c7bea6aae8120480f727076f464
Craig <craigdanj@gmail.com>
2020-03-26 00:09:28 +0530

    additional code for ejs template

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(shopRoutes);
 
 app.use((req, res ,next) => {
     //The 404 route
-    res.status(404).render('404');
+    res.status(404).render('404', { pageTitle: "Page Not Found"});
 });
 
 app.listen(3000);
